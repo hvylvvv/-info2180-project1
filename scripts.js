a28b7a1c121d91c3ffe627fd9cb38d254fbe0806
Aalyah <jhonsonaalyah@gmail.com>
2023-11-05 19:50:51 -0500

    js file added

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
-/* Add your JavaScript to this file */
+window.addEventListener("load", (event) => {
+    const emailForm = document.querySelector("form");
+    const msg = document.querySelector(".message");
+  
+    emailForm.addEventListener("submit", function (e) {
+      e.preventDefault();
+  
+      const emailAddress = document.getElementById("email").value;
+  
+      // Check that email address is valid
+      if (emailAddress !== "") {
+        msg.textContent =
+          "Thank you! Your email address " +
+          emailAddress +
+          " has been added to our mailing list!";
+  
+        emailInput.value = "";
+      } else {
+        msg.textContent = "Please enter a valid email address.";
+      }
+    });
+  });
+  
